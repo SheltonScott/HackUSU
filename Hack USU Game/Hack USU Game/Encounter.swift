4ce21fdd69c61a0b04d68c7c9e219469cbff6fa0
alcow5 <alcow5@gmail.com>
2018-11-17 03:32:08 -0700

    added some new assets and files

---
+++ Hack USU Game/Hack USU Game/Encounter.swift
@@ -0,0 +1,22 @@
+//
+//  Encounter.swift
+//  Hack USU Game
+//
+//  Created by Alex  Cowley on 11/17/18.
+//  Copyright © 2018 scott shelton. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import GameplayKit
+
+class Encounter: SKScene {
+    //This is the encounter scene
+    override func didMove(to view: SKView) {
+        <#code#>
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
+    }
+}
